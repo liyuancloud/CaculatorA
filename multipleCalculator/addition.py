38bfa864ee3cafc31d16ef092685f3535c335aee
AKA-Nordcloud <asle.apeland@nordcloud.com>
2022-03-18 12:59:03 +0100

    Added code for addition function

--- multipleCalculator/addition.py
+++ multipleCalculator/addition.py
@@ -1,4 +1,8 @@
 import functions_framework
 @functions_framework.http
 def addition_http(request):
-    ...
+    request_json = request.get_json(silent=True)
+    request_args = request.args
+
+def addition(a,b):
+    return a+b
