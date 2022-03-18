2e2d7a5c82ac676139c853f3bb42027462912a47
liyuancloud <101333858+liyuancloud@users.noreply.github.com>
2022-03-18 14:04:14 +0200

    Modify subtraction function

--- multipleCalculator/subtraction.py
+++ multipleCalculator/subtraction.py
@@ -1,4 +1,20 @@
 import functions_framework
 @functions_framework.http
-def substraction_http(request):
-    ...
+
+def subtraction_http(request):
+    
+    """HTTP Cloud Function.
+    Args:
+        a  - the a value
+        b  - the b value
+    Returns:
+        The computed value
+    """
+    
+    request_json = request.get_json(silent=True)
+    request_args = request.args
+    
+    return f"The result of subtraction is: subtraction(a,b)"
+
+def subtraction(a,b):
+    return a - b
